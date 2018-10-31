c956b8a84d6f8d70b6ad80116bbd1690900be8db
Marcelo Morgado <cmarcelom@gmail.com>
2018-10-31 22:44:36 +0000

    test tools installation

---
+++ src/tests/index.js
@@ -0,0 +1,22 @@
+import { expect, assert } from "chai";
+import fetch from 'node-fetch';
+
+describe("assets", () => {
+
+	it("renders without crashing (shallow)", () => {
+		
+	});
+
+	// it("renders a <Button /> component", () => {
+	// 	const { button } = setup();
+	// 	expect(button).to.have.lengthOf(1);
+	// });
+	//
+	//
+	// it("simulates click events", () => {
+	// 	const { props, button } = setup();
+	// 	button.simulate("click");
+	// 	expect(props.onClick).to.have.property("callCount", 1);
+	// 	assert(props.onClick.withArgs(props.enimyId).calledOnce);
+	// });
+});
